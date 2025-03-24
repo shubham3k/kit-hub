0dc3cd0b3138e750a704252841e958bf3adb838a
Shubham3k <shubhamkushwaha2536@gmail.com>
2025-03-24 22:41:20 +0530

    appwrite lib support

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -1,14 +1,49 @@
-import { Client, Account, Databases, Storage } from 'appwrite';
+"use server";
+import { Client, Account } from "node-appwrite";
+import { cookies } from "next/headers";
 
-const client = new Client();
+export async function createSessionClient() {
+  const client = new Client()
+    .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT || "")
+    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT || "");
 
-client
-    .setEndpoint('NEXT_PUBLIC_APPWRITE_HOST_URL')
-    .setProject('NEXT_PUBLIC_APPWRITE_PROJECT_ID');
+  const sessionCookies = await cookies();
+  const session = sessionCookies.get("my-custom-session");
+  if (!session || !session.value) {
+    throw new Error("No session");
+  }
 
-export const account = new Account(client);
-export const databases = new Databases(client);
-export const storage = new Storage(client);
+  client.setSession(session.value);
 
-export { client };
+  return {
+    get account() {
+      return new Account(client);
+    },
+  };
+}
+
+export async function createAdminClient() {
+  const client = new Client()
+    .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT || "")
+    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT || "")
+    .setKey(process.env.NEXT_PUBLIC_APPWRITE_API_KEY || "");
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+  };
+}
+
+
+export async function getLoggedInUser() {
+    try {
+      const { account } = await createSessionClient();
+      return await account.get();
+    } catch (error) {
+      return null;
+    }
+  }
+  
+
 
